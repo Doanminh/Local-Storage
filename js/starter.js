f190144961316e799c6a6a235980fb406d79d543
Doanminh <32391115+Doanminh@users.noreply.github.com>
2018-01-05 15:42:23 -0500

    done

---
+++ js/starter.js
@@ -0,0 +1,10 @@
+let contactStarter =[
+
+ {"fullname": "Minh Doan", "phone" :"343-988-7487", "email" : "doan0028@algonquinlive.com"},   
+    
+//{"fullname": "Hans Solo", "phone" :"613-727-4723", "email" : "han.solo@falcon.com"},
+//
+//{"fullname": "Luke Skywalker"," phone" :"613-321-8765","email":"luke.skywalker@edi.org"},
+//
+//{"fullname": "Kylo Ren", " phone" :"613-876-9400", "email" : "kylo.re@firstorder.com"}
+];
